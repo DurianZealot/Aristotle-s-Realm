d833415e7588cf40efa5efe5498a8995a52499da
DurianZealot <yolandazik@qq.com>
2020-10-29 03:00:27 +0800

    Initialize hardcoded data

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,3 +1,4 @@
+import { ThreeSixty } from '@material-ui/icons';
 import React from 'react';
 import {Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
@@ -5,9 +6,29 @@ import Home from './components/Home'
 import Registration from './components/Registration'
 import Search from './components/Search/Search';
 import UserProfile from './components/UserProfile'
+import storyData from './data/stories';
+
+const authorId="authorId", storyline="storyLine", storyPreview="storyPreview", lastUpdate="lastUpdate";
+
  class App extends React.Component{
+  
+  
+   
+  constructor(props) {
+    // initialize a set of story data 
+    // userIds are unique
+    // each story has a unique story name, author userId, storyLine, storyPreview, lastUpdate date
+    super(props);
+    this.state = {
+      userIds : ["LUsCH", "piPHe", "Ioust", "mairT"],  
+      stories : storyData
+      }
+    }
+
+  
 
   render() {
+    console.log(this.state)
     return (
       <div>
         <BrowserRouter>
@@ -15,7 +36,7 @@ import UserProfile from './components/UserProfile'
             <Route exact path='/' render={() => (<Home />)}/>
             <Route exact path='/register' render={() => (<Registration/>)} />
             <Route exact path='/profile/user' render={() => (<UserProfile/>)} />
-            <Route exact path='/search' render={() => (<Search/>)}/>
+            <Route exact path='/search' render={() => (<Search data={this.state.stories}/>)}/>
           </Switch>
         </BrowserRouter>
       </div>
