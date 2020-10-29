afc7a22b03f13f3c3ae3cd8ceb7ad1a73692df9c
Tianyi Zhang <psyminette.zhang@mail.utoronto.ca>
2020-10-29 14:30:21 -0400

    cleaned up code for UserProfile

--- webapp/src/components/UserProfile/index.js
+++ webapp/src/components/UserProfile/index.js
@@ -1,91 +1,70 @@
-import React from 'react';
-import SideBar from './../SideBar'
-import "./styles.css"
+import React from "react";
+import SideBar from "./../SideBar";
+import GeneralInfo from "./../GeneralInfo";
+import SocialStats from "./../SocialStats";
+import CareerStats from "./../CareerStats";
+import "./styles.css";
 
+class UserProfile extends React.Component {
+  state = {
+    user: {
+      name: "Ipsum Lorem",
+      iconPath: "icon/profile-icon-placeholder.png",
+      age: "20",
+      genrePref: "Sci-Fi",
 
-class UserProfile extends React.Component{
-    render() {
-        const {user} = this.props // User Object to be used to grab all the data
-        return (
-            <div className="user-profile">
-                <SideBar isLoggedIn={true}>
-                
-                </SideBar>
-                {/* Header */}
-                <div className="user-profile__header blocks">
-                    <div className="user-profile__icon">
-                        {/* Currently the css file HARDCODES a placeholder profile image */}
-                        {/* Requires a server call to get the user's profile icon */}
-                    </div>
-                    <div className="user-profile__basic-info">
-                        <h1 className="user-profile__username text">
-                            {/* Currently HARDCORDED */}
-                            {/* Requires a server call */}
-                            FirstName LastName
-                        </h1>
-                        <h3 className="user-profile__age text">
-                            {/* Currently HARDCODED */}
-                            Age: 20
-                        </h3>
-                        <h3 className="user-profile__preferred-genre text">
-                            {/* Currently HARDCODED */}
-                            Preferred Genre: Sci-Fi
-                        </h3>
-                    </div>
-                </div>    
-                {/* General Information*/}
-                <div className="user-profile__info-container blocks">
-                        <div className="user-profile__info-table">
-                            <div className="user-profile__info-item">
-                                {/* Currently HARDCORDED */}
-                                <div className="item-title text">Joined:</div>
-                                <div className="item-value text">December 21</div>
-                            </div>
-                            <div className="user-profile__info-item">
-                                {/* Currently HARDCORDED */}
-                                <div className="item-title text">Followers:</div>
-                                <div className="item-value text">6666</div>
-                            </div>
-                            <div className="user-profile__info-item">
-                                {/* Currently HARDCORDED */}
-                                <div className="item-title text">Followed:</div>
-                                <div className="item-value text">420</div>
-                            </div>
-                            <div className="user-profile__info-item">
-                                {/* Currently HARDCORDED */}
-                                <div className="item-title text">Approval Rate:</div>
-                                <div className="item-value text">69%</div>
-                            </div>
-                        </div>
-                </div>
-                <div className="career-featured-works blocks">
-                        <h1 className="career-featured-works-header text">
-                            Featured Works:
-                        </h1>
-                        {/* Currently HARDCODED */}
-                        <div className="career-featured-works-item text">
-                            This user hasn't published any works yet...
-                        </div>
-                </div>
-                {/* Novelist Career */}
-                {/* Currently HARDCODED */}
-                <div className="user-profile__career-stats">
-                    <h2 className="career-stats-header text">
-                        Career Statistics:
-                    </h2>
-                    <div className="career-stats-item text">
-                        Number of Proposals Accepted: 9
-                    </div>
-                    <div className="career-stats-item text">
-                        Number of works begun: 11
-                    </div>
-                    <div className="career-stats-item text">
-                        Last Contributed On: Oct 31, 2020
-                    </div>
-                </div>
-            </div>
-        )
-    }
+      joinDate: "December 21",
+      followerCount: "6666",
+      followingCount: "420",
+      approvalRate: "69",
+
+      proposalAcceptNum: "9",
+      worksBegunNum: "11",
+      LastContributionDate: "Oct 31, 2020"
+    },
+    isLoggedIn: false,
+  };
+
+  // If this component is able to load then the user must have logged in
+  componentDidMount() {
+    // When the component enters the DOM
+    this.setState({
+      isLoggedIn: true,
+    });
+  }
+
+  render() {
+    return (
+      <div className="user-profile">
+        <SideBar isLoggedIn={this.state.isLoggedIn} />
+        <GeneralInfo
+          name={this.state.user.name}
+          iconPath={this.state.user.iconPath}
+          age={this.state.user.age}
+          genrePref={this.state.user.genrePref}
+        />
+
+        <SocialStats
+          joinDate={this.state.user.joinDate}
+          followerCount={this.state.user.followerCount}
+          followingCount={this.state.user.followingCount}
+          approvalRate={this.state.user.approvalRate}
+        />
+        <div className="career-featured-works blocks">
+          <h1 className="career-featured-works-header text">Featured Works:</h1>
+          {/* Currently HARDCODED */}
+          <div className="career-featured-works-item text">
+            This user hasn't published any works yet...
+          </div>
+        </div>
+        <CareerStats
+            proposalAcceptNum={this.state.user.proposalAcceptNum}
+            worksBegunNum={this.state.user.worksBegunNum}
+            LastContributionDate={this.state.user.LastContributionDate}
+        />
+      </div>
+    );
+  }
 }
 
-export default UserProfile
+export default UserProfile;
