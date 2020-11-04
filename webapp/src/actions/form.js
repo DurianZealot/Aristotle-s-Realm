fe4775a7d0636c427c7e4f9f21787a57b30cc7da
Tony Hong <tonyh12879@gmail.com>
2020-11-04 18:37:56 -0500

    refactored login page function so that event methods are in the actions folder under form.js

---
+++ webapp/src/actions/form.js
@@ -0,0 +1,24 @@
+export const handleInputChange = (event, form) => {
+    form.setState({
+        [event.target.name]: event.target.value
+    })
+}
+export const handleSubmit = (form) => {
+    const {username, password} = form.state
+    const userArray = form.props.appState.users.filter(user => {
+        return user.username === username && user.password === password
+    })
+    const currentUser = userArray[0]
+    form.props.appState.currentUser = currentUser
+    if (currentUser){
+        form.setState(
+            {
+                redirect: true
+            }
+        )
+    }
+    else {
+        document.getElementById('errorText').appendChild(document.createTextNode('Incorrect Username or Password'))
+    }        
+       
+}
--- webapp/src/components/Registration/index.js
+++ webapp/src/components/Registration/index.js
@@ -1,14 +1,1 @@
-import React from 'react'
-
-class Registration extends React.Component{
-    render() {
-        return(
-            <div>
-                <h1>
-                    Welcome to the registration page
-                </h1>
-            </div>
-        )
-    }
-}
-export default Registration
+// 
