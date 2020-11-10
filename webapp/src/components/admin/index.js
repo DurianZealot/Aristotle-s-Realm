19c6070ad18589eaea8f1a27c3ced9c04b6e361c
Tony Hong <tonyh12879@gmail.com>
2020-11-09 21:59:07 -0500

    finished admin view wireframe

--- webapp/src/components/admin/index.js
+++ webapp/src/components/admin/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
-
+import {Link} from 'react-router-dom'
+import {Button} from '@material-ui/core';
 class Admin extends React.Component {
     constructor(props) {
         // When the component is created
@@ -12,8 +13,31 @@ class Admin extends React.Component {
     render() {
 
         return (
-            <div>
-                This is the admin view
+            <div className='admin_page'>
+            
+                <div className='admin_title_container'> 
+                    <Link className="admin_logout-link" to={"/"} onClick={() => {this.props.appState.currID = null; this.props.appState.currentUser = null; window.sessionStorage.clear()}}> {/* Log User Out */}
+                        <Button className="button" color='primary' variant="contained" size="large">Logout</Button>
+                    </Link>
+                    <span className='admin_title_text'>
+                        ADMIN DASHBOARD
+                    </span>
+                </div>    
+                <div className='stories_container'> 
+                    <span className='admin_stories_text'>
+                        All Users
+                    </span>
+                </div> 
+                <div className='users_container'> 
+                    <span className='admin_users_text'>
+                        All Stories
+                    </span>
+                </div> 
+                <div className='reports_container'> 
+                    <span className='admin_reports_text'>
+                        All Reports
+                    </span>
+                </div> 
             </div>
         );
     }
