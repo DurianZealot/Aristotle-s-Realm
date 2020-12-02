0258045e7004ba61c2dd0d389d130cfa2e48b605
Tony Hong <tonyh12879@gmail.com>
2020-12-02 17:41:41 -0500

    connected to database and added sample api call

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const express = require("express");
 const app = express();
 const path = require('path')
 
-// // mongoose and mongo connection
-// const { mongoose } = require("./db/mongoose");
-// mongoose.set('useFindAndModify', false); // for some deprecation issues
+// mongoose and mongo connection
+const { mongoose } = require("./mongoose");
+mongoose.set('useFindAndModify', false); // for some deprecation issues
+
+
+//user model TODO: change from generic example
+const{ User } = require("./models/user")
 
 // body-parser: middleware for parsing HTTP JSON body into a usable object
 const bodyParser = require("body-parser");
@@ -20,6 +24,46 @@ const session = require("express-session");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "/client/build")));
+
+function isMongoError(error) { // checks for first error returned by promise rejection if Mongo database suddently disconnects
+    return typeof error === 'object' && error !== null && error.name === "MongoNetworkError"
+}
+
+// middleware for mongo connection error for routes that need it
+const mongoChecker = (req, res, next) => {
+    // check mongoose connection established.
+    if (mongoose.connection.readyState != 1) {
+        log('Issue with mongoose connection')
+        res.status(500).send('Internal server error')
+        return;
+    } else {
+        next()  
+    }   
+}
+
+//making a user (test API call)
+app.post('/api/users', mongoChecker, async (req, res) => {
+    log(req.body)
+
+    // Create a new user
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    })
+
+    try {
+        // Save the user
+        const newUser = await user.save()
+        res.send(newUser)
+    } catch (error) {
+        if (isMongoError(error)) { // check for if mongo server suddenly disconnected before this request.
+            res.status(500).send('Internal server error')
+        } else {
+            log(error)
+            res.status(400).send('Bad Request') // bad request for changing the student.
+        }
+    }
+})
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/client/build/index.html"));
