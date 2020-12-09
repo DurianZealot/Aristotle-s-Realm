450c17cf83e87b9a68b31ae7b360ccea3df802fb
DurianZealot <yolandazik@qq.com>
2020-12-10 06:43:51 +0800

    create action for proposal

---
+++ client/src/actions/proposal.js
@@ -0,0 +1,11 @@
+"use strict"
+
+import Axios from "axios"
+
+const log = console.log
+
+export const postProposal = async function(proposeToID, proposeToTile, proposeToTile, proposeChapter, visibility, content, proposalByUsername, status = 'pending'){
+    Axios.post()
+    
+
+}
